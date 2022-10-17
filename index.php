9776546a49fd87bdb8540bf495e374f6effb5b30
Tegarluqman <109190581+Tegarluqman@users.noreply.github.com>
2022-10-17 08:15:17 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,18 +1,70 @@
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-          <meta charset="UTF-8">
-          <meta http-equiv="X-UA-Compatible" content="IE=edge">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>slebew Website</title>
 </head>
-<body><center>
-          <h1>RUMAH SAKIT</h1>
-          <h1>SEHAT WARAS SAYUNG</h1>
-          <h3><a href=profil.php>Profil</a><h3>
-          <h3><a href=pasien.php>Lihat pasien</a><h3>
-          <h3><a href=tambah.php>tambah pasien</a><h3>
-          <h3><a href=fasilitas.php>fasilitas</a><h3>
-          </center>
+
+<body>
+    <nav>
+        <a href="profil.php">Profil</a> |
+        <a href="pasien.php">Lihat pasien</a> |
+        <a href="tambah.php">Tambah pasien</a> |
+        <a href="fasilitas.php">Fasilitas</a>
+    </nav>
+
+    <hr />
+
+    <header style="text-align: center;">
+        <img src="Rumah-sakit.jpg" width="200" height="200" style="border-radius: 50%;"/>
+        <h1>RUMAH SAKIT </h1>
+        <h1>SEHAT WARAS SAYUNG</h1>
+    </header>
+
+    <hr />
+
+    <article style="text-align: center;">
+        <h2>Overview</h2>
+        <p>
+            Selamat datang di website resmi RUMAH SAKIT SEHAT WARAS SAYUNG
+        </p>
+    </article>
+
+    <div style="max-width: 600px; margin: 3em auto">
+        <table border="1" width="100%">
+            <thead>
+                <tr>
+                    <th>Skill</th>
+                    <th>Pengalaman</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <ul>
+                            <li>HTML (Expert)</li>
+                            <li>CSS (Beginner)</li>
+                            <li>Javascript (Beginner)</li>
+                        </ul>
+                    </td>
+                    <td>
+                        <ul>
+                            <li>Freelancer di Internet</li>
+                            <li>Leaeder Local Linux Community</li>
+                            <li>Leaeder Local Linux Community</li>
+                        </ul>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <hr>
+    <footer style="text-align: center;">
+        <p>Copyright &copy; 2022 Tegar Luqman.</p>
+    </footer>
 </body>
 </html>
