078529cd4c6d6fb49c731c34a61387845614f598
Tegarluqman <109190581+Tegarluqman@users.noreply.github.com>
2022-10-20 08:32:10 +0700

    Add files via upload

---
+++ fasilitas.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Document</title>
+</head>
+<body>
+          <p>RS PMC memiliki fasilitas yang dapat memenuhi kebutuhan pasien secara menyeluruh. Fasilitas yang ada berbasis komputerisasi dan sistem on line sehingga mempercepat waktu proses pelayanan kepada pasien. Fasilitas-fasilitas ini diselenggarakan secara optimal dengan orientasi kepuasan pelanggan. Fasilitas- fasilitas yang tersedia di RS PMC adalah sebagai berikut :</p>
+<br>
+<p>Rawat Jalan</p>
+<p>Unit Gawat Darurat</p>
+<p>Rawat Inap</p>
+<p>ICU</p>
+<p>Kamar Operasi</p>
+<p>Medical Check Up</p>
+<p>Farmasi</p>
+<p>Ambulance</p>
+<p>Fisioterapi</p>
+<p>Laboratorium</p>
+<p>Radiologi (USG,Dental X-Ray)</p>
+<p>Drill Evacuation</p>
+</body>
+</html>
---
+++ profil.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Document</title>
+</head>
+<body>
+          <h1>Rumah Sakit merupakan Lembaga Pemerintah yang menjalankan fungsi
+kesehatan yakni memberikan sarana dasar, upaya kesehatan rujukan dan upaya
+kesehatan penunjang, dimana dalam penyelenggaraan harus memperhatikan fungsi
+sosial.</h1>
+<br>
+<h2>Sebagai fungsi sosial di bidang kesehatan, Rumah Sakit Umum Daerah Cilacap
+selanjutnya disingkat RSUD Cilacap merupakan pelayanan publik yang senantiasa
+melakukan pemasaran atas peran, fungsi dan manajemen Rumah Sakit. Oleh karena
+RSUD Cilacap terus berbenah diri untuk mengembangkan kualitas Manajemen
+Rumah Sakit, melaksanakan tugas dan fungsi rumah sakit secara profesional.</h2>
+</body>
+</html>
